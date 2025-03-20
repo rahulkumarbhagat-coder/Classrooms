1ed432a67cb8832b681eee1609a3c1d4f75095e1
Gallie83 <keving2913@hotmail.com>
2025-03-20 15:56:52 -0700

    Login/Register pages now match the designs, also added ResetPassword/NewPassword pages for users who forgot their password.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import GenerateQuizForm from './pages/GenerateQuizForm.jsx';
 import Homepage from './pages/HomePage.jsx';
 import Register from './pages/AuthPages/Register.jsx'
 import Login from './pages/AuthPages/Login.jsx'
+import ResetPassword from './pages/AuthPages/ResetPassword.jsx';
+import NewPassword from './pages/AuthPages/NewPassword.jsx';
 import CreateClassroom from './pages/ClassroomPages/CreateClassroom.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import JoinClassroom from './pages/ClassroomPages/JoinClassroom.jsx';
@@ -34,6 +36,14 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <Login />
+      },
+      {
+        path: '/reset-password',
+        element: <ResetPassword />
+      },
+      {
+        path: '/new-password',
+        element: <NewPassword />
       },
       {
         path: '/create-classroom',
